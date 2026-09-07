b48da48aeaf7d2a026792482d7c6d51b9614f68e
Mike Seese <seesemichaelj@gmail.com>
2026-09-07 14:01:44 -0700

    use updated smial.org domain

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-export const NTFY_TOPIC = "mike_seese_weather_alert";
-export const SITE_URL = "https://wind.seese.net";
+export const SITE_URL = "https://wind.smial.org";
 export const WIND_THRESHOLD_KN = 8;
 export const GUST_THRESHOLD_KN = 15;
